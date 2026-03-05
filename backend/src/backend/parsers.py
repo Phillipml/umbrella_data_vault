43e6d06053d330022414bc9bdf47965e320cdebe
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-05 15:40:48 -0300

    create function to get name and img src from characters

--- backend/src/backend/parsers.py
+++ backend/src/backend/parsers.py
@@ -9,18 +9,49 @@ if html == "Error":
 
 soup = BeautifulSoup(html, features="html.parser")
 
-find_birth_date = soup.find("em", string=re.compile("Ano de nascimento"))
-if find_birth_date:
-    character_div_content = find_birth_date.find_parent("div")
 
-    character_ems = character_div_content.find_all("em")
+def get_name(soup):
+    name = soup.find("span", string=re.compile("Personagens"))
+    if name:
+        text = name.get_text(strip=True)
+        result = text.replace(" | ", ",").split(",", 1)
+        return result[1]
+    return None
 
+
+def get_profile_image(soup):
+    img = soup.find("img")
+    if img and img.get("src"):
+        img_src = img["src"]
+        return {"img_src": img_src}
+    return None
+
+
+def get_basic_infos(soup):
     character_data = {}
-    for i in character_ems:
-        text = i.get_text(strip=True)
-        if ":" in text:
-            key, value = text.split(":")
-            character_data[key.strip()] = value.strip()
-character_data
 
+    find_birth_date = soup.find("em", string=re.compile("Ano de nascimento"))
+
+    if find_birth_date:
+        character_name = get_name(soup)
+        character_img = get_profile_image(soup)
+
+        if character_name:
+            character_data["name"] = character_name
+        if character_img:
+            character_data["img_src"] = character_img["img_src"]
+        character_div_content = find_birth_date.find_parent("div")
+        character_ems = character_div_content.find_all("em")
+
+        for i in character_ems:
+            text = i.get_text(strip=True)
+
+            if ":" in text:
+                key, value = text.split(":", 1)
+                character_data[key.strip()] = value.strip()
+
+    return character_data
+
+
+get_basic_infos(soup)
 # %%
