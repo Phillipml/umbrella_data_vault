2586288ea9941d64a5cca39b839d03dfa543e64b
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-05 15:02:10 -0300

    create function to find the character with name requisition

--- backend/src/backend/parsers.py
+++ backend/src/backend/parsers.py
@@ -1,23 +1,26 @@
 # %%
+from backend.scraper import get_character_content
 from bs4 import BeautifulSoup
 import re
-from backend.scraper import html
+
+html = get_character_content("Jill Valentine")
+if html == "Error":
+    print("Erro na requisição, verificar se o nome do personagem está correto")
 
 soup = BeautifulSoup(html, features="html.parser")
-soup.find("div", class_="td-page-content")
 
 find_birth_date = soup.find("em", string=re.compile("Ano de nascimento"))
 if find_birth_date:
     character_div_content = find_birth_date.find_parent("div")
 
-character_ems = character_div_content.find_all("em")
+    character_ems = character_div_content.find_all("em")
 
-character_data = {}
-for i in character_ems:
-    text = i.get_text(strip=True)
-    if ":" in text:
-        key, value = text.split(":")
-        character_data[key.strip()] = value.strip()
+    character_data = {}
+    for i in character_ems:
+        text = i.get_text(strip=True)
+        if ":" in text:
+            key, value = text.split(":")
+            character_data[key.strip()] = value.strip()
 character_data
 
 # %%
--- backend/src/backend/scraper.py
+++ backend/src/backend/scraper.py
@@ -1,13 +1,16 @@
 # %%
 from config import cookies, headers
+import re
 import requests
 
-res = requests.get(
-    "https://www.residentevildatabase.com/personagens/ada-wong/",
-    cookies=cookies,
-    headers=headers,
-)
-# %%
-res.status_code
-# %%
-html = res.text
+
+def get_character_content(name: str):
+    character = re.sub(r"[(),.]", "", name.strip().lower()).replace(" ", "-")
+    res = requests.get(
+        f"https://www.residentevildatabase.com/personagens/{character}/",
+        cookies=cookies,
+        headers=headers,
+    )
+    if res.status_code != 200:
+        return "Error"
+    return res.text
