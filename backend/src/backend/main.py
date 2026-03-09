ce2777bdf62fa130cc63218986f87225380c0d8e
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-09 15:22:33 -0300

    fast api setup

---
+++ backend/src/backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from backend.parsers.character_list import get_all_characters
+from backend.parsers.character_detail import character_data
+
+app = FastAPI()
+
+
+@app.get("/characters_list")
+def character_list():
+    return get_all_characters()
+
+
+@app.get("/character-bio/{param}")
+def get_character_bio(param: str):
+    return character_data(param)
