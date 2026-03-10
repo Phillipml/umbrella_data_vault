570fdb5e68ad2d9a3bf28cd6f91228d7e8140e41
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-10 00:46:31 -0300

    provider

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers/Providers";
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -18,11 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`antialiased ${montserrat.className}`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Image from "next/image";
+import { useGetCharactersListQuery } from "./lib/api";
 
 export default function Home() {
+  const {data}=useGetCharactersListQuery()
   return (
     <div className="min-h-screen">
       <div className="max-w-[1400px] m-auto">
@@ -8,6 +11,9 @@ export default function Home() {
         <Image src="/umbrella-full-logo.png" alt="logo" width={180} height={32} style={{objectFit: "contain"}}/>
         </div>
         <div className="w-15 animate-ping"><Image src="/umbrella-icon.png" alt="logo" width={180} height={32} style={{objectFit: "contain"}}/></div>
+        {data?.map(i =>(
+          <p>{i.name}</p>
+        ))}
       
       </div>
     </div>
