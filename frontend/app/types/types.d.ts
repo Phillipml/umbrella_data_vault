046ee5dbcf2f68b199199bd0ba4c8883ecb5a451
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-09 23:10:35 -0300

    api and types

---
+++ frontend/app/types/types.d.ts
@@ -0,0 +1,16 @@
+type Character = {
+    name: string,
+    param: string
+}
+
+export type CharacterList = Character[]
+
+export type CharacterData = {
+    name: string,
+    img: string,
+    birth: string,
+    bloodType: string,
+    height: string,
+    weight: string,
+    bio:string
+}
