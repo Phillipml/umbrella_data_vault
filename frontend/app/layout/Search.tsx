94615cb3d823658087370ab3a491c5c6cfa8d90a
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-13 11:55:19 -0300

    Search input created

--- frontend/app/layout/Search.tsx
+++ frontend/app/layout/Search.tsx
@@ -1,3 +1,19 @@
-export default function Search() {
-  return <></>
+type SearchProps = {
+  value: string
+  onChange: (value: string) => void
+  placeholder?: string
+}
+export default function Search({
+  value,
+  onChange,
+  placeholder = 'Buscar...'
+}: SearchProps) {
+  return (
+    <input
+      type="search"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  )
 }
