6cf21a56e2af6cc446f7cb8ea71c922a3a051611
Phillipml <contato.phillip.menezes@gmail.com>
2026-03-13 13:44:14 -0300

    Search useMemo working

--- frontend/app/layout/List.tsx
+++ frontend/app/layout/List.tsx
@@ -1,8 +1,19 @@
 import Image from 'next/image'
 import { useGetCharactersListQuery } from '../lib/api'
+import { useMemo, useState } from 'react'
+import Search from './Search'
 
 export default function List() {
   const { data, isError, isLoading } = useGetCharactersListQuery()
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const filteredList = useMemo(() => {
+    if (!data) return []
+    if (!searchTerm.trim()) return data
+    const term = searchTerm.toLowerCase().trim()
+    return data.filter((i) => i.name.toLowerCase().includes(term))
+  }, [data, searchTerm])
+  const isSearch = searchTerm == ''
   if (isError) {
     return 'Erro ao carregar Lista'
   }
@@ -21,16 +32,29 @@ export default function List() {
     )
   }
   return (
-    <div className="w-2/4 h-100 m-auto flex flex-wrap justify-center gap-x-12 gap-y-4 overflow-y-scroll mt-10 mb-10 scrollbar-custom lg:w-full">
-      {data?.map((i) => (
-        <a
-          key={i.param}
-          href={`/${i.param}`}
-          className="w-80 mb-2 border-b border-alternative rounded-2xl text-center cursor-pointer hover:shadow-md hover:shadow-alternative hover:text-shadow-lg/30 hover:text-shadow-alternative"
-        >
-          {i.name}
-        </a>
-      ))}
-    </div>
+    <>
+      <Search value={searchTerm} onChange={setSearchTerm} />
+      <div className="w-2/4 h-100 m-auto flex flex-wrap justify-center gap-x-12 gap-y-4 overflow-y-scroll mt-10 mb-10 scrollbar-custom lg:w-full">
+        {isSearch
+          ? data?.map((i) => (
+              <a
+                key={i.param}
+                href={`/${i.param}`}
+                className="w-80 mb-2 border-b border-alternative rounded-2xl text-center cursor-pointer hover:shadow-md hover:shadow-alternative hover:text-shadow-lg/30 hover:text-shadow-alternative"
+              >
+                {i.name}
+              </a>
+            ))
+          : filteredList?.map((i) => (
+              <a
+                key={i.param}
+                href={`/${i.param}`}
+                className="w-80 h-6 mb-2 border-b border-alternative rounded-2xl text-center cursor-pointer hover:shadow-md hover:shadow-alternative hover:text-shadow-lg/30 hover:text-shadow-alternative"
+              >
+                {i.name}
+              </a>
+            ))}
+      </div>
+    </>
   )
 }
